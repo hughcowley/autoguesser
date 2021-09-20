d99f70bcab916204db82067101466cbdc5f341f7
Hugh Cowley <hjcowley@gmail.com>
2021-09-20 11:29:52 +0100

    Version 0.1 Initial script Return number of guesses needed to reach target

--- autoguesser_main.py
+++ autoguesser_main.py
@@ -1,1 +1,34 @@
-#init
+# AutoGuesser 0.1
+# by Hugh Cowley
+# https://github.com/hughcowley
+#
+# This program selects a random number N between 0 and 100
+# then guesses a number x between 0 and 100
+# if x is [higher/lower] than N, make a new guess [lower/higher] than x
+# repeat until x = N
+# return the number of guesses needed to reach x = N
+
+from random import randint
+
+def main():
+    """Main program function"""
+
+    # initialise parameters
+    N = randint(1, 100)
+    x = randint(1, 100)
+    g = 1
+
+    while x != N:
+        if x > N:
+            g += 1
+            x = randint(N, x)
+        else:
+            g+= 1
+            x = randint(x, N)
+
+    print(f"{g} guesses taken.")
+
+    input("Press the Enter key to exit.")
+
+if __name__ == '__main__':
+    main()
